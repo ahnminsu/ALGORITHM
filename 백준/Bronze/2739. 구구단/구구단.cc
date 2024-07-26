fea6baab35e3e97fb5dcd02a3bf8c563f4ff4b73
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-07-26 22:17:04 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int num;
+	int i;
+	
+	cin>>num;
+	
+	for (i=1;i<10;i++){
+		cout<<num<<" * "<<i<<" = "<<num*i<<endl;
+	}
+	
+	return 0;
+ 
+}
