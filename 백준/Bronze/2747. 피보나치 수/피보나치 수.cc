00e366fc805db2074deff53208f00e63f3f54963
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-07-26 22:17:58 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int d[45] = {0,};
+
+int fibo (int n) {
+    if(n==0) return 0;
+    else if(n==1) return 1;
+    else if (d[n] != 0) return d[n];
+    d[n]=fibo(n-1)+fibo(n-2);
+    return d[n];
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fibo(n);
+   return 0;
+}
