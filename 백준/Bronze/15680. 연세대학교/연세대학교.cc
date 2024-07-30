328cf96002bab6148ae1d996c84962ccbdc1b92f
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-07-30 23:57:31 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num;
+
+    cin>>num;
+
+    if(num==0) cout<<"YONSEI";
+    else if (num==1) cout<<"Leading the Way to the Future";
+
+    return 0;
+}
