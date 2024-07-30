5585eca2f5f00113123f8882a17f580ea6d46b45
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-07-30 23:59:48 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int byte;
+	int num;
+	int i;
+	
+	cin>>byte;
+	
+	num=byte/4;
+	
+	for(i=0;i<num;i++){
+		cout<<"long ";
+	}
+	
+	cout<<"int";
+	
+	return 0;
+ 
+}
