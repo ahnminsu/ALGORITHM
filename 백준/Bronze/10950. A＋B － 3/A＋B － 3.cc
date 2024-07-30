7d819ab5cb3885ab8c01e6d5a2708e5fa56eb2c5
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-07-30 23:52:51 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	int num,a,b;
+	int i;
+	
+	cin>>num;
+	
+	for(i=0;i<num;i++){
+		cin>>a>>b;
+		cout<<a+b<<endl;
+	}
+	
+	return 0;
+ 
+}
