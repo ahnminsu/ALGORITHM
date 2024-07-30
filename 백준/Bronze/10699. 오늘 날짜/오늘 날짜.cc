f0b31dfb3ee1584b4ebc75488c0d56038a484c79
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-07-30 22:22:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+
+    time_t t = time(NULL);
+    struct tm *now = localtime(&t);
+
+    cout<< now->tm_year+1900<<"-";
+
+    cout.width(2);
+    cout.fill('0');
+    cout<<now->tm_mon+1<<"-";
+    
+    cout<<now->tm_mday;
+
+
+    return 0;
+}
