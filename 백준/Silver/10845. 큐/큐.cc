47d27fafb3b278908de49ef32e65d86dcc49ea8e
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-08-19 22:18:09 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    queue<int>q;
+    int n,num;
+    string str;
+
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>str;
+        if(str == "push"){
+            cin>>num;
+            q.push(num);
+        }
+        else if(str == "pop"){
+            if(q.empty()){
+                cout<<"-1"<<"\n";
+            }
+            else{
+                cout<<q.front()<<"\n";
+                q.pop();
+            }
+        }
+        else if (str=="size"){
+            cout<<q.size()<<"\n";
+        }
+        else if (str == "empty"){
+            cout<<q.empty()<<"\n";
+        }
+        else if (str =="front"){
+            if(q.empty()){
+                cout<<"-1"<<"\n";
+            }
+            else{
+                cout<<q.front()<<"\n";
+            }
+        }
+        else if (str =="back"){
+            if(q.empty()){
+                cout<<"-1"<<"\n";
+            }
+            else{
+                cout<<q.back()<<"\n";
+            }
+        }
+    }
+
+
+    return 0;
+}
