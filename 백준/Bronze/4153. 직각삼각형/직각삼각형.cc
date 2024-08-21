7b2fad062838c96f4f8ea6134643cae5a94267ee
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-08-21 23:24:22 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a,b,c;
+
+    while(cin>>a>>b>>c){
+
+        if(a==0&&b==0&&c==0){
+            return 0;
+        }
+
+        if(a*a==b*b+c*c||b*b==a*a+c*c||c*c==a*a+b*b){
+            cout<<"right"<<"\n";
+        }
+        else{
+            cout<<"wrong"<<"\n";
+        }
+    }
+
+    return 0;
+}
