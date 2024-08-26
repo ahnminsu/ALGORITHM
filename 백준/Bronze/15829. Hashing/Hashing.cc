07b60a4b21333296de3a28de418b11ce117775be
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-08-27 00:29:49 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int L;
+    int hash = 0;
+    int R = 1;
+    int r = 31;
+    int M = 1234567891;
+    char arr[51];
+
+    cin>>L;
+
+    for(int i=0;i<L;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<L;i++){
+        hash += ((arr[i] -'a' +1) * R) % M;
+        R *= r;
+        hash %= M;
+        R %= M;
+    }
+
+    cout<<hash;
+
+    return 0;
+}
