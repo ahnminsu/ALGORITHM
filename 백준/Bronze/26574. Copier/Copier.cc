cd9df05d5b140205605075e3a3ab1a9cb391f785
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-08-29 00:10:56 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,a;
+
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>a;
+        cout<<a<<" "<<a<<"\n";
+    }
+
+    return 0;
+}
