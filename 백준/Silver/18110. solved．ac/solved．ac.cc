be75b66f6cd4d65f32fa2d9835f15b353b95d977
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2024-09-29 23:44:42 +0900

    [Silver IV] Title: solved.ac, Time: 32 ms, Memory: 3072 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int level[300000];
+    int n;
+    cin>>n;
+
+    if(n==0){
+        cout<<0;
+        return 0;
+    }
+
+    for(int i=0;i<n;i++){
+        cin>>level[i];
+    }
+
+    sort(level,level+n);
+
+    int cnt = round(n * 0.15);
+    int sum =0;
+
+    for(int i=cnt;i<n-cnt;i++){
+        sum +=  level[i];
+    }
+
+    int res = round((double)sum / (n-cnt*2));
+
+    cout<<res;
+
+    return 0;
+}
