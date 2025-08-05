5ae5ec99eedc3accb39935c4058f96eb30e8cf69
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2025-08-05 13:48:58 +0900

    [Silver V] Title: K번째 수, Time: 1024 ms, Memory: 21428 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n,k;
+    cin>>n>>k;
+
+    int arr[n];
+
+    for (int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    cout<<arr[k-1];
+
+
+
+    return 0;
+}
