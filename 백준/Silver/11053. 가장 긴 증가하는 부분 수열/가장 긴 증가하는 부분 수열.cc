4892f9a7c669c3b80a5f5ba657a28a3f132fb60d
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2025-11-05 13:57:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int LTS(vector<int>&arr){
+    vector<int>lis;
+    for (int i:arr) {
+       auto it = lower_bound(lis.begin(),lis.end(),i);
+        if (it ==lis.end()) lis.push_back(i);
+        else *it=i;
+    }
+    return (int)lis.size();
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+
+    for (int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+
+    cout<<LTS(arr);
+
+    return 0;
+}
