cfe16942e56870e5a2b65b3373ab902eb60d628d
ahnminsu <165612851+ahnminsu@users.noreply.github.com>
2025-11-15 22:04:20 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,22 +1,19 @@
 #include<iostream>
+
 using namespace std;
 
-int main(){
-	
-	int score;
-	
-	cin>>score;
+int main() {
 
-	if (score<=100&&score>=90)
-		cout<<"A";
-	else if (score<=89&&score>=80)
-		cout<<"B";
-	else if (score<=79&&score>=70)
-		cout<<"C";
-	else if (score<=69&&score>=60)
-		cout<<"D"; 
-	else  
-		cout<<"F";  
-		
-	return 0;
+    int n;
+
+    cin>>n;
+
+    if (n>=90) cout<<"A";
+    else if (n>=80) cout<<"B";
+    else if (n>=70) cout<<"C";
+    else if (n>=60) cout<<"D";
+    else cout<<"F";
+
+
+    return 0;
 }
